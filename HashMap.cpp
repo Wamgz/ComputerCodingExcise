e45475c3dab004fe174a967aa1ca854d9a995e5c
wang <623585585@qq.com>
2020-09-21 19:31:33 +0800

    detach definition of class and some other methods from initial class

--- HashMap.cpp
+++ HashMap.cpp
@@ -1,17 +1,13 @@
-#include <iostream>
-#include <cstring>
-#include"FileOption.h"
+#include "HashMap.h"
 using namespace std;
 
-//RShash映射函数 
 unsigned int RSHash(char *str)
 {
 	unsigned int b = 378551;
 	unsigned int a = 63689;
 	unsigned int hash = 0;
 
-	while (*str)
-	{
+	while (*str){
 		hash = hash * a + (*str++);
 		a *= b;
 	}
@@ -19,65 +15,45 @@ unsigned int RSHash(char *str)
 	return (hash & 0x7FFFFFFF);
 }
 
-//链表元素定义 
-struct ListNode {
-	char* val;
-	ListNode* next;
-	ListNode() : val(NULL), next(NULL) {}
-	ListNode(char* x) : val(x), next(NULL) {}
-	ListNode(char* x, ListNode* next) : val(x), next(next) {}
-};
-struct HeadNode{
-	int val;
-	ListNode* next;
-	HeadNode() : val(0), next(NULL){}
-	HeadNode(int x) : val(x), next(NULL){}
-};
 
-//hashmap 定义 
-class HashMap{
-	private:
-		HeadNode hash[13];
+HashMap::HashMap(){
+	for(int i = 0;i < hash_len;i++){
+		hash[i] = HeadNode();
+	}
+}
+
+int HashMap::get(char* val){
+	int hashId = RSHash(val);
+	hashId %= hash_len;
+	HeadNode head = hash[hashId];
+	ListNode* p = head.next;
+
+	while(p != NULL){
+		if(strcmp(p -> val, val) != 0){
+			p = p -> next;
+		}
+		else{
+			return 1;
+		}
+	} 	
+	return 0;
+}
 		
-	public:
-		HashMap(){
-			for(int i = 0;i < 13;i++){
-				hash[i] = HeadNode();
-			}
-		}
-		int get(char* val){
-			int hashId = RSHash(val);
-			hashId %= 13;
-			HeadNode head = hash[hashId];
-			ListNode* p = head.next;
+void HashMap::put(char* val){
+	int hashId = RSHash(val);
+	hashId %= hash_len;
+	
+	HeadNode* head = &hash[hashId];
+	ListNode* p = new ListNode(val);
+	if(head -> next == NULL){
+		head -> next = p;
+	}
+	else{
+		p -> next = head -> next;
+		head -> next = p;				
+	}
+}
 
-			while(p != NULL){
-				if(strcmp(p -> val, val) != 0){
-					p = p -> next;
-				}
-				else{
-					return 1;
-				}
-			} 
-			
-			return 0;
-		}
-		
-		void put(char* val){
-			int hashId = RSHash(val);
-			hashId %= 13;
-
-			HeadNode* head = &hash[hashId];
-			ListNode* p = new ListNode(val);
-			if(head -> next == NULL){
-				head -> next = p;
-			}
-			else{
-				p -> next = head -> next;
-				head -> next = p;				
-			}
-		}
-};
 int main(){
 	HashMap* map = new HashMap();
 	FileReader* fr = new FileReader();
---
+++ HashMap.h
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstring>
+#define hash_len 11003
+
+unsigned int RSHash(char *str); 
+
+struct ListNode {
+	char* val;
+	ListNode* next;
+	ListNode() : val(NULL), next(NULL) {}
+	ListNode(char* x) : val(x), next(NULL) {}
+	ListNode(char* x, ListNode* next) : val(x), next(next) {}
+};
+struct HeadNode{
+	int val;
+	ListNode* next;
+	HeadNode() : val(0), next(NULL){}
+	HeadNode(int x) : val(x), next(NULL){}
+};
+
+class HashMap{
+	private:
+		HeadNode hash[11003];
+		
+	public:
+		HashMap();
+		int get(char* val);
+		void put(char* val);
+};
