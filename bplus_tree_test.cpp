44661945d231ae9e0a28e8b4143128dac7d947a8
woods <623585585@qq.com>
2020-10-11 23:05:18 +0800

    原始的测试数据

---
+++ bplus_tree_test.cpp
@@ -0,0 +1,86 @@
+int main(){	
+	//插入数据需要环境保证是UTF-8编码
+	//读取文件需要保证环境是GB2312编码
+
+	// BPlusTree* tree = new BPlusTree(3);
+	// tree -> insert("a", "a");
+	// tree -> insert("b", "b");
+	// tree -> insert("c", "c");
+	// tree -> insert("d", "d");
+	// tree -> insert("h", "h");
+	// tree -> insert("es", "es");
+	// tree -> insert("ffdsf", "ffdsf");
+	// tree -> insert("g", "g");
+	// tree -> insert("i", "i");
+	// tree -> insert("j", "j");
+	// tree -> insert("ksd", "ksd");
+	// tree -> insert("mgh", "mgh");
+
+	// tree -> insert("l", "l");
+	// tree -> insert("n", "n");
+	// tree -> insert("o", "o");
+	// tree -> insert("s", "s");
+
+	// tree -> insert("p", "p");
+	// tree -> insert("q", "q");
+	// tree -> insert("r", "r");
+	// tree -> insert("API标准", "API标准");
+
+	// tree -> insert("ASTM标准", "ASTM标准");
+	// tree -> insert("A形井架", "A形井架");
+	// tree -> insert("BZ28-1油田", "BZ28-1油田");
+	// tree -> insert("东", "东");
+
+	// tree -> insert("井", "井");
+
+	// tree -> insert("北", "北");
+
+
+	// tree -> insert("南", "南");
+	// tree -> insert("氢", "氢");
+	// tree -> insert("西", "西");
+
+
+	// tree -> insert("DC指数", "DC指数");
+
+	// // tree -> printValues();
+	// // tree -> printKeys();
+	// tree -> insert("F-K域滤波", "F-K域滤波");
+	// tree -> insert("F-X域", "F-X域");
+	// // tree -> printKeys();
+
+	// tree -> insert("IPR曲线", "IPR曲线");
+	// tree -> insert("ISO标准", "ISO标准");
+	// tree -> insert("K-L变换", "K-L变换");
+	// tree -> insert("K-L滤波", "K-L滤波");
+	// tree -> insert("K形井架", "K形井架");
+	// tree -> insert("LNG船", "LNG船");
+	// tree -> insert("PH值", "PH值");
+	// tree -> insert("X射线", "X射线");
+	// tree -> insert("X射线分析", "X射线分析");
+	// tree -> insert("X射线探伤", "X射线探伤");
+	// tree -> insert("X射线衍射", "X射线衍射");
+	// tree -> insert("X射线探伤仪", "X射线探伤仪");	
+	// tree -> insert("X射线式分析器", "X射线式分析器");
+	// tree -> insert("X射线衍射分析", "X射线衍射分析");
+	// tree -> insert("Z变换", "Z变换");
+	// tree -> insert("Z电位", "Z电位");
+	// tree -> insert("dc指数", "dc指数");	
+	// tree -> insert("图", "图");
+	// tree -> insert("塔", "塔");
+	// tree -> insert("柱", "柱");
+	// tree -> insert("桥", "桥");
+	// tree -> insert("桩", "桩");
+
+	// tree -> printValues();
+	// tree -> printKeys();
+	// // tree -> printValues();
+
+
+	// // cout<<tree -> find("es", "es")<<endl;
+	// // cout<<tree -> find("ffdsf", "ffdsf")<<endl;	
+	// // cout<<tree -> find("s", "s")<<endl;
+	// // cout<<tree -> find("zsd", "zsd")<<endl;
+	// // cout<<tree -> find("东", "东")<<endl;
+	// // cout<<tree -> find("冰", "冰")<<endl;
+}
